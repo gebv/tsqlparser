248af92d12b7e36c2c7aa9cb444cb5a6b75dd74d
Egor Bachurin <egor.internet@gmail.com>
2015-08-28 10:14:41 +0600

    init

---
+++ main.go
@@ -0,0 +1,128 @@
+package main
+
+import (
+	"code.google.com/p/go-charset/charset"
+  	_ "code.google.com/p/go-charset/data"
+  	"regexp"
+	"encoding/xml"
+	"encoding/json"
+	"bytes"
+	"log"
+	"io/ioutil"
+	"flag"
+	"strings"
+	"fmt"
+)
+
+type Event struct {
+	ObjectName string `xml:"ObjectName,attr"`
+	DatabaseName string `xml:"DatabaseName,attr"`
+	Value  string `xml:",chardata"`
+}
+
+type Events struct {
+	XMLName xml.Name  `xml:events`
+  	Events   []Event `xml:"event"`
+
+}
+
+type PageProfile struct {
+	PageName string `json:"page_name"`
+
+	SqlQueres []SqlProfile `json:"sql_queres"`
+}
+
+type SqlProfile struct {
+	SeqNumber int `json:"sequence_number"`
+	SqlFormat string `json:"sql_format"`
+	SqlParams string `json:"sql_params"`
+	SqlQuery string `json:"sql_query"`
+}
+
+var filePath string
+
+func init() {
+	flag.StringVar(&filePath, "f", "", "path file")
+}
+
+func main() {
+	flag.Parse()
+
+	log.Printf("INFO: Open file '%s'", filePath)
+
+	// Открыли файл
+	data, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Выбрали все SQL запросы с параметрами
+	var events = Events{}
+	var page = new(PageProfile)
+	
+	reader := bytes.NewReader(data)
+
+	decoder := xml.NewDecoder(reader)
+	decoder.CharsetReader = charset.NewReader
+	err = decoder.Decode(&events)
+	if err != nil {
+		log.Printf("ERROR: Decode xml. %s", err)
+		return
+	}
+	// Записали все SQL запросы с вставленными параметрами
+
+	for _sqlSeqNumber, event := range events.Events {
+		if event.ObjectName == "sp_prepexec" {
+			sql := SqlProfile{}
+			sql.SeqNumber = _sqlSeqNumber
+			
+			// Находим SQL и параметры
+			// Кол-во параметров в SQL должно равняться кол-ву параметров
+			// reSQL := regexp.MustCompile("'(select.+?)'")
+			reSQL := regexp.MustCompile(`N'(select.+?)'`)
+			findSQL := reSQL.FindStringSubmatch(event.Value)
+
+			if len(findSQL) > 0 {
+				sql.SqlFormat = strings.Trim(findSQL[1], " ");
+
+				// reRemoveParame := regexp.MustCompile("@P\\d+");
+				// reRemoveParame.ReplaceAllString(src, )
+
+				reParams := regexp.MustCompile(`N'select.+?',(.*)`)
+				findParams := reParams.FindStringSubmatch(event.Value)
+
+				if len(findParams) == 0 {
+					continue
+				}
+
+				sql.SqlParams = strings.Trim(findParams[1], " ")
+
+				paramsArray := strings.Split(sql.SqlParams, ",")
+
+				sql.SqlQuery = sql.SqlFormat
+
+				for index, param := range paramsArray {
+
+					var value string
+
+					switch {
+					case string(param[0]) == "N":
+						value = param[1:len(param)]
+					default: 
+						value = param
+					}
+
+					sql.SqlQuery = strings.Replace(sql.SqlQuery, fmt.Sprintf("@P%d", index), value, -1)
+				}
+			}
+
+			page.SqlQueres = append(page.SqlQueres, sql)
+		}
+	}
+
+	pageBytes, _ := json.Marshal(page)
+
+	ioutil.WriteFile(fmt.Sprintf("%s.sql_profiles.json", filePath), pageBytes, 0644)
+	log.Println("Done")
+	
+}
